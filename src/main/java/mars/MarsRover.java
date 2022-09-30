9e6f0e79b4211728d725ad3206a0e73c1f571edf
adrixo <adrianvalrom@gmail.com>
2022-09-30 12:32:33 +0200

    feat: whole rotation right

--- src/main/java/mars/MarsRover.java
+++ src/main/java/mars/MarsRover.java
@@ -13,6 +13,10 @@ public class MarsRover {
                 direction = "E";
             else if (direction == "E")
                 direction = "S";
+            else if (direction == "S")
+                direction = "W";
+            else if (direction == "W")
+                direction = "N";
         }
         return "0:0:" + direction;
     }
